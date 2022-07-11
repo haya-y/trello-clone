e7fdc50a65b822298639de5786d9d3b150ff0cc6
hayate_y <hayate.bgu.senyo@gmail.com>
2022-07-12 02:00:22 +0900

    [Delete] 余分な記述の削除

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import React, { useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import React from "react";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { TaskCards } from "./components/task/TaskCards";
 
 function App() {
-  const [items] = useState([...Array(3)].map((_, i) => ({ id: i, text: `item${i}` })));
-
-  const onDragEnd = (result) => {
-    const remove = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, remove[0]);
-  };
-
   return (
     <div className="app">
       <Header />
       <TaskCards />
-
-      {/* <div className="dragDropArea">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided) => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                {items.map((item, index) => (
-                  <Draggable draggableId={item.text} index={index} key={item.id}>
-                    {(provided) => (
-                      <div
-                        className="item"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        {item.text}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div> */}
     </div>
   );
 }
